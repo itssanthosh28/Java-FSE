1f9c12468caa99671894e1b6ff38315d72b39ea3
itssanthosh28 <marksanthosh2004@gmail.com>
2025-06-29 08:18:15 +0530

    Add files via upload

---
+++ Week 2/JUnit_Basic Testing Exercises/Exercise 1 -Setting Up JUnit/JUnitSetupExample/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        int result = calc.add(3, 7);
+        assertEquals(10, result);
+    }
+}
